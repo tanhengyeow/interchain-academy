a27fddb5ec91641dcc032d6dd6a6e413635604ff
tanhengyeow <E0032242@u.nus.edu>
2022-09-30 15:48:27 +0800

    feat: add new message createGame

--- ts-client/alice.checkers.checkers/registry.ts
+++ ts-client/alice.checkers.checkers/registry.ts
@@ -1,6 +1,8 @@
 import { GeneratedType } from "@cosmjs/proto-signing";
+import { MsgCreateGame } from "./types/checkers/tx";
 
 const msgTypes: Array<[string, GeneratedType]>  = [
+    ["/alice.checkers.checkers.MsgCreateGame", MsgCreateGame],
     
 ];
 
