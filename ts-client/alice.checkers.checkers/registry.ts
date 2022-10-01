d6b196a3f314505249827226eb3344a540121e3b
tanhengyeow <E0032242@u.nus.edu>
2022-10-01 17:12:58 +0800

    feat: add ways to make a move + test

--- ts-client/alice.checkers.checkers/registry.ts
+++ ts-client/alice.checkers.checkers/registry.ts
@@ -1,8 +1,10 @@
 import { GeneratedType } from "@cosmjs/proto-signing";
 import { MsgCreateGame } from "./types/checkers/tx";
+import { MsgPlayMove } from "./types/checkers/tx";
 
 const msgTypes: Array<[string, GeneratedType]>  = [
     ["/alice.checkers.checkers.MsgCreateGame", MsgCreateGame],
+    ["/alice.checkers.checkers.MsgPlayMove", MsgPlayMove],
     
 ];
 
